05f1a2986ffa96413007999c5ef5321118552eeb
Natali Kamenkova <natalikamenkova@gmail.com>
2022-12-19 18:45:10 +0200

    1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const http=require('http');
 const app = require('./app');
-const PORT = 3000;
+//const PORT = 3000;
+const port= process.env.PORT;
 const server = http.createServer(app);
-server.listen(PORT, ()=>{console.log("server started")})
+server.listen(port, ()=>{console.log("server started")})
